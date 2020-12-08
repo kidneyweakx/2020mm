227f87435653d4367740de1e7fbb0f93e4dc3a17
kidneyweakx <kidneyweakx@gmail.com>
2020-12-08 10:34:01 +0800

    🙄 Fix unofficial Max time sync

---
+++ contest/max/helpers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from time import time as _time
+from datetime import datetime
+
+def get_current_timestamp():
+    # print(datetime.utcnow().timestamp, int(round(_time() * 1000))
+    return int(round(_time() * 1000))
